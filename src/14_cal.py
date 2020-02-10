c11700a1e9fd23d8bb0fcc510606d6592567a52b
Victor <arowovici@gmail.com>
2020-02-10 16:12:43 +0100

    Completed MVP Requirements

--- src/14_cal.py
+++ src/14_cal.py
@@ -18,15 +18,25 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('--m', type=str, help='Month',
+                    default=datetime.now().month)
+parser.add_argument('--y', type=str, help='Year',
+                    default=datetime.now().year)
+
+args = parser.parse_args()
+print(calendar.month(int(args.y), int(args.m)))
